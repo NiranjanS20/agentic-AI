e538a4c1679275f94ffdc2cb9f5bf775bfe7eabd
Niranjan Shanbhag <nshanbhag345@gmail.com>
2026-05-02 18:15:42 +0530

    Update embeddings.py

---
+++ rag/embeddings.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def embed_chunks(text_chunks):
+    return model.encode(text_chunks)
+
+def embed_query(query):
+    return model.encode([query])[0]
